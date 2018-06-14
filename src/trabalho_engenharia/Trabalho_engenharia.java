0f3be243728937edf19744c599e50aabff97b7b2
Fernando <Fernando@LAPTOP-6E0PD0DU>
2018-06-14 14:50:30 -0300

    Commit Fernando

---
+++ src/trabalho_engenharia/Trabalho_engenharia.java
@@ -0,0 +1,13 @@
+
+package trabalho_engenharia;
+
+
+public class Trabalho_engenharia {
+
+    public static void main(String[] args) {
+        
+        nomesView nomes = new nomesView();
+        
+    }
+    
+}
---
+++ src/trabalho_engenharia/nomesView.java
@@ -0,0 +1,49 @@
+
+package trabalho_engenharia;
+
+
+public class nomesView extends javax.swing.JFrame {
+
+   
+    public nomesView() {
+        initComponents();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel1.setText("JANELA DO FERNANDO");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(34, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(32, 32, 32))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(100, 100, 100)
+                .addComponent(jLabel1)
+                .addContainerGap(124, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
+}
